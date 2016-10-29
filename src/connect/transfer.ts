e553efbca3d25021df0200c79dfc8e76912200f8
Joris Blaak <joris@label305.com>
2016-10-29 20:26:51 +0200

    Refactored connecting to Express (#8)

---
+++ src/connect/transfer.ts
@@ -0,0 +1,31 @@
+import {Application, Request, Response, NextFunction} from "express";
+
+/**
+ * Transfer the different methods to the Express application
+ * @param app
+ * @param method
+ * @param path
+ * @param middleware
+ * @param controller
+ */
+export default function transfer(app: Application,
+                                 method: string,
+                                 path: string,
+                                 middleware: Array<(req: Request, res: Response, next: NextFunction) => any>,
+                                 controller: ((req: Request, res: Response) => void)) {
+    switch (method) {
+        case 'get':
+            app.get(path, middleware, controller);
+            break;
+        case 'post':
+            app.post(path, middleware, controller);
+            break;
+        case 'put':
+            app.put(path, middleware, controller);
+            break;
+        case 'delete':
+            app.delete(path, middleware, controller);
+            break;
+    }
+};
+
---
+++ src/util/trim.ts
@@ -0,0 +1,9 @@
+/**
+ * Trim slashes of the left of a string
+ * @param str
+ * @returns {string}
+ */
+export function trim(str: string): string {
+    return str.replace(/^\/|\/$/g, '');
+};
+
