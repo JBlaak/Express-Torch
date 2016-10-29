3890b69193d2cb81358d49c7d443431600bbb560
Joris Blaak <joris@label305.com>
2016-10-29 21:13:59 +0200

    Added typings to npm package

--- src/torch.ts
+++ src/torch.ts
@@ -1,6 +1,12 @@
 import {Application} from "express";
 import Router from "./router";
+import {RouteConfig} from "./models/route_config";
+import {GroupConfig} from "./models/group_config";
 import connect from "./connect";
+
+export type Router = Router;
+export type Route = RouteConfig;
+export type Group = GroupConfig;
 
 export default function Torch(app: Application, callback: (router: Router) => void) {
     const router = new Router();
