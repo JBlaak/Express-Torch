5293f1bd0bd03255512f64d0f0aa996ed7590528
Dirk-Jan <dirk.jan.wassink@gmail.com>
2017-04-04 13:05:00 +0200

    Allow usage of router level error handling middleware.

--- src/models/middleware.ts
+++ src/models/middleware.ts
@@ -1,3 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
 
-export type Middleware = (req: Request, res: Response, next: NextFunction) => any;
+export type NormalMiddleware = (req: Request, res: Response, next: NextFunction) => any;
+export type ErrorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => any;
+
+export type Middleware = NormalMiddleware | ErrorMiddleware;
